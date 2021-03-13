3d9473ab9a228788f1e6e1825c88927fc3cc4fdb
priyanshu chauhan <priyanshuc.info@gmail.com>
2021-03-13 23:59:35 +0530

    Added few media queries

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.scss";
 import { RiChatDeleteLine, RiChatCheckLine, RiAddLine } from "react-icons/ri";
+import { motion, AnimatePresence } from "framer-motion";
 const App = () => {
     const [textField, setTextField] = useState("");
     const [todoList, setTodoList] = useState([]);
@@ -9,11 +10,10 @@ const App = () => {
     useEffect(() => {
         let todo = JSON.parse(sessionStorage.getItem("todoList"));
         let done = JSON.parse(sessionStorage.getItem("doneList"));
-        if (todo.length > 0) setTodoList(todo);
-        if (done.length > 0) setDoneList(done);
+        if (todo) setTodoList(todo);
+        if (done) setDoneList(done);
     }, []);
 
-    
     const todoSubmit = (e) => {
         e.preventDefault();
         if (textField !== "")
@@ -37,49 +37,6 @@ const App = () => {
     }, [todoList, doneList]);
     return (
         <div className="todo-container">
-            <div className="list">
-                <div className="list__todo">
-                    {todoList &&
-                        todoList.map((list) => (
-                            <li className="list__todo__li" key={list.uid}>
-                                {list.list}
-                                <div>
-                                    <RiChatDeleteLine
-                                        onClick={() => deleteTodo(list.uid)}
-                                        className="list__todo__li__button"
-                                        style={{ color: "red" }}
-                                    />
-                                    <RiChatCheckLine
-                                        onClick={() =>
-                                            doneTodo(list.uid, list.list)
-                                        }
-                                        className="list__todo__li__button"
-                                        style={{ color: "blue" }}
-                                    />
-                                </div>
-                            </li>
-                        ))}
-                </div>
-                <div className="list__done">
-                    {doneList &&
-                        doneList.map((list) => (
-                            <li
-                                className="list__todo__li dashed"
-                                style={{ textDecoration: "line-through" }}
-                                key={list.uid}
-                            >
-                                {list.list}
-                                <div>
-                                    <RiChatDeleteLine
-                                        onClick={() => deleteTodo(list.uid)}
-                                        className="list__todo__li__button"
-                                        style={{ color: "red" }}
-                                    />
-                                </div>
-                            </li>
-                        ))}
-                </div>
-            </div>
             <div className="input-area">
                 <h1 className="input-area__heading">Enter your to-dos</h1>
                 <form onSubmit={todoSubmit} className="input-area__form">
@@ -97,6 +54,62 @@ const App = () => {
                         className="input-area__form__submit"
                     />
                 </form>
+            </div>
+            <div className="list">
+                <div className="list__todo">
+                    <AnimatePresence>
+                        {todoList &&
+                            todoList.map((list) => (
+                                <motion.li
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    className="list__todo__li"
+                                    key={list.uid}
+                                >
+                                    {list.list}
+                                    <div>
+                                        <RiChatDeleteLine
+                                            onClick={() => deleteTodo(list.uid)}
+                                            className="list__todo__li__button"
+                                            style={{ color: "red" }}
+                                        />
+                                        <RiChatCheckLine
+                                            onClick={() =>
+                                                doneTodo(list.uid, list.list)
+                                            }
+                                            className="list__todo__li__button"
+                                            style={{ color: "blue" }}
+                                        />
+                                    </div>
+                                </motion.li>
+                            ))}
+                    </AnimatePresence>
+                </div>
+                <div className="list__done">
+                    <AnimatePresence>
+                        {doneList &&
+                            doneList.map((list) => (
+                                <motion.li
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    className="list__todo__li dashed"
+                                    style={{ textDecoration: "line-through" }}
+                                    key={list.uid}
+                                >
+                                    {list.list}
+                                    <div>
+                                        <RiChatDeleteLine
+                                            onClick={() => deleteTodo(list.uid)}
+                                            className="list__todo__li__button"
+                                            style={{ color: "red" }}
+                                        />
+                                    </div>
+                                </motion.li>
+                            ))}
+                    </AnimatePresence>
+                </div>
             </div>
         </div>
     );
